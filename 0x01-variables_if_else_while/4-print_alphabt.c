ea1273a66d84992558894cb745564d854d94c4e4
AymericMbepa <aymericmbepa@gmail.com>
2022-09-10 10:19:42 -0700

    print alphabets except 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch1 = 'a';
+while (ch1 <= 'z' && ch1 != 'q' && ch1 != 'e')
+{
+putchar(ch1);
+ch1++;
+}
+return (0);
+}
