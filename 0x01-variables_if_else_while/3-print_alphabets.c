9e9522f6062bf98afa3fd78532e94e2803ebd6a0
AymericMbepa <aymericmbepa@gmail.com>
2022-09-10 09:50:29 -0700

    print all alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,17 @@ int main(void)
 {
 char ch1 = 'a';
 char ch2 = 'A';
-while (ch1 <= 'z', ch <= 'Z')
+while (ch1 <= 'z')
 {
 putchar(ch1);
-putchar(ch2);
 ch1++;
+}
+while (ch2 <= 'Z')
+{
+putchar(ch2);
 ch2++;
+}
+ptchar('\n');
+return (0);
 }
  
