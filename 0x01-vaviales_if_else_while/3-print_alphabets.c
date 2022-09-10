af19ac694d33a368ff993cb2dba48eef620335d0
AymericMbepa <aymericmbepa@gmail.com>
2022-09-10 09:15:24 -0700

    print all alphabets

---
+++ 0x01-vaviales_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch1, ch2 = 'a', 'A';
+while (ch1 <= 'z', ch2 <= 'Z')
+{
+putchar(ch1);
+putchar(ch2);
+ch1++;
+ch2++;
+}
+return (0);
+}
