24ae0f2280924a8537126fd817ed97eb7e5e36e1
AymericMbepa <aymericmbepa@gmail.com>
2022-09-11 04:09:31 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'z';
+while (ch <= 'z')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
