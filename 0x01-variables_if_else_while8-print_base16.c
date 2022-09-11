fde908a68a8adb61089721a840f4ba5e0e4eca70
AymericMbepa <aymericmbepa@gmail.com>
2022-09-11 04:33:53 -0700

    print base 16

---
+++ 0x01-variables_if_else_while8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit = '0';
+char letter = 'a';
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+} 
+while (letter <= 'f')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+} 
