272f40960cb65ba78bde19d22f51cbeb12f02c82
AymericMbepa <aymericmbepa@gmail.com>
2022-09-13 15:17:05 -0700

    check for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,16 @@
 
 /**
  * _islower - check for lowercase character
- *
+ * @c: An input character
  * Return: 1 if it is and 0 otherwise
  */
 int _islower(int c)
 {
 char ch;
-int value;
+int value = 0;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 if (c == ch)
 value = 1;
-else
-value = 0;
-}
 return (value);
 }
