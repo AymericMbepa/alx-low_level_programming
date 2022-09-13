735be30461419c07fa6b9ffa834130d4a8b0ec20
AymericMbepa <aymericmbepa@gmail.com>
2022-09-13 15:48:20 -0700

    checks for alphabetic characte

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,9 @@ int _isalpha(int c)
 char ch1;
 char ch2;
 int value = 0;
-for (ch1 = 'a', ch2 = 'A' ; ch1 <= 'z', ch2 <= 'Z' ; ch1++, ch2++)
+for (ch1 = 'a', ch2 = 'A' ; ch1 <= 'z' && ch2 <= 'Z' ; ch1++, ch2++)
 {
-if (value == ch1 || value == ch2)
+if (c == ch1 || c == ch2)
 value = 1;
 }
 return (value);
