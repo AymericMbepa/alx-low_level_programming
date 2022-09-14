82bc228673539f938191556d275c84f9f1f2ca70
AymericMbepa <aymericmbepa@gmail.com>
2022-09-14 12:32:33 -0700

    print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,15 @@
  */
 int print_last_digit(int c)
 {
-int last_digit;
-if (c >= 0)
-{
-last_digit = c % 10;
-}
-else
-last_digit = -1 * (c % 10);
-_putchar(last_digit);
-_putchar('\n');
-return (last_digit);
+  int last_digit;
+
+  if (c < 0)
+    last_digit = -1 * (c % 10);
+  else
+    last_digit = c % 10;
+
+  _putchar(last_digit);
+  _putchar('\n');
+
+  return (last_digit % 10);
 }
