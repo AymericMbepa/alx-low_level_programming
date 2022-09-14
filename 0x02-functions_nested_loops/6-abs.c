529e4e8ba656f582bd3f8bdc42d849029606a4e0
AymericMbepa <aymericmbepa@gmail.com>
2022-09-14 01:26:51 -0700

    print absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,15 @@
 
 /**
  * _abs - compute the absolute value of an integer
+ *@c: an input parameter
  * Return: Absolute value of an integer
  */
-int _abs(int)
+int _abs(int c)
 {
-int value = _abs();
-if (_abs() < 0)
+int value = c;
+if (c < 0)
 {
-value = -_abs();
+value = -c;
 }
 return (value);
 }
