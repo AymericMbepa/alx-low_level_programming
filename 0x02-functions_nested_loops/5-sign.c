604a0ed12fd1041d4dc2cab9a90600a9fd8100d6
AymericMbepa <aymericmbepa@gmail.com>
2022-09-15 02:05:12 -0700

    function test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,21 @@
  */
 int  print_sign(int n)
 {
-int value = 1;
-if (n < 0)
-{
-value = -1;
-_putchar('-');
-}
-else
-{
-if (n == 0)
-{
-value = 0;
-_putchar('0');
-}
-else
-_putchar('+');
-}
-return (value);
+	int value = 1;
+	if (n < 0)
+	{
+		value = -1;
+		_putchar('-');
+	}
+	else
+	{
+		if (n == 0)
+		{
+			value = 0;
+			_putchar('0');
+		}
+		else
+			_putchar('+');
+	}
+	return (value);
 }
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+ This projest is about debugging which  is the process of finding and fixing
+ errors in software that prevents it for running correctly
