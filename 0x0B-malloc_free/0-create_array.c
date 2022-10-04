3c6cd8b7036f188310fec4fd124b9f624093ee22
AymericMbepa <aymericmbepa@gmail.com>
2022-10-04 04:39:20 -0700

    create an array of char and initialize whit specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,14 +22,11 @@ char *create_array(unsigned int size, char c)
 
 	if (array == NULL)
 		return (NULL);
-	else
-	{
-		while (i < size)
-		{
-			array[i] = c;
-			i++;
-		}
-	}
 
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
 	return (array);
 }
