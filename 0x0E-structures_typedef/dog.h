3a8cf5bb831d82394392babfd9bc4467cb789d14
AymericMbepa <aymericmbepa@gmail.com>
2022-10-11 03:30:18 -0700

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef _POPPY_H_
 #define _POPPY_H_
+
+/**
+ * struct dog - Define a new type of element
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Descrption: structurethat contents personnal data of a person
+ */
+
 struct dog
 {
 	char *name;
