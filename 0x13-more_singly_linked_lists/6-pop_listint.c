d6e8fb0527bc83349b51bf2e76ff2474076eebc4
AymericMbepa <aymericmbepa@gmail.com>
2022-10-24 14:18:02 -0700

    case of NULL input

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,12 +12,12 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
 
 	n = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
-	(*head) = temp
+	*head = temp;
 	return (n);
 }
