abb13f5174560bf6a10535e14b62ddaae9220cf4
AymericMbepa <aymericmbepa@gmail.com>
2022-10-24 13:45:46 -0700

    free old head space

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,15 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *temp;
 	int n;
 
 	if (head != NULL)
 	{
+		temp = (*head);
 		n = (*head)->n;
 		(*head) = (*head)->next;
+		free(temp);
 	}
 
 	return (n);
