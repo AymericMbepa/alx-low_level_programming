b629650b65398c5f6bb80a50378cd1e75c47b10f
AymericMbepa <aymericmbepa@gmail.com>
2022-11-02 04:06:11 -0700

    creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,9 @@
 #include <sys/uio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
